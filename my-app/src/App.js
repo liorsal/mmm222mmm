6ed4dd9db44740a1574fe50883b1bcbfabf8134c
Lior Salmanovich <lior92626@gmail.com>
2024-12-22 23:21:23 +0200

    d

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UploadReport from './components/UploadReport';
 import Insights from './components/Insights';
 import Overview from './components/Overview';
@@ -17,11 +17,11 @@ function App() {
           </nav>
         </header>
         <main>
-          <Switch>
-            <Route path="/upload" component={UploadReport} />
-            <Route path="/insights" component={Insights} />
-            <Route path="/" component={Overview} />
-          </Switch>
+          <Routes>
+            <Route path="/upload" element={<UploadReport />} />
+            <Route path="/insights" element={<Insights />} />
+            <Route path="/" element={<Overview />} />
+          </Routes>
         </main>
       </div>
     </Router>
